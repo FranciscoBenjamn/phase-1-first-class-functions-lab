58b6ba00549f87d3fe0201cfd18ca444b93ca1fc
Franklin Benjamin <franciscovibes14@gmail.com>
2022-05-05 13:24:09 -0400

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return [drivers[0],drivers[1]]
+}
+
+function returnLastTwoDrivers(drivers){
+    return [drivers[drivers.length - 2],drivers[drivers.length - 1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+  return function(value) {
+      return multiplier * value
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn){
+    if (fn == returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(drivers)
+    }
+    else if (fn == returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)
+    }
+}
